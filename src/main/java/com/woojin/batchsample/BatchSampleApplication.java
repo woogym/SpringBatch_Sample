89efd75f07e6938ea0de57c4f8b60e2093c8e98d
woogym <kwj9294@gmail.com>
2025-09-01 19:12:02 +0900

    initial commit

---
+++ src/main/java/com/woojin/batchsample/BatchSampleApplication.java
@@ -0,0 +1,13 @@
+package com.woojin.batchsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchSampleApplication.class, args);
+    }
+
+}
