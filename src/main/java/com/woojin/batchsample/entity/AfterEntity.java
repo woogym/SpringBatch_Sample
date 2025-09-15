45ec7f5133582b6957a01f92f43f3dfbf7b01141
woogym <kwj9294@gmail.com>
2025-09-16 01:38:16 +0900

    배치 실습1: 실습 데이터 삽입을 위한 AfterEntity setter 추가

--- src/main/java/com/woojin/batchsample/entity/AfterEntity.java
+++ src/main/java/com/woojin/batchsample/entity/AfterEntity.java
@@ -15,4 +15,15 @@ public class AfterEntity {
     private Long id;
 
     private String userName;
+
+    public void setUserName(String userName) {
+        validateUserName(userName);
+        this.userName = userName;
+    }
+
+    private void validateUserName(String userName) {
+        if (userName.isBlank() || userName.isEmpty()) {
+            throw new IllegalArgumentException("이름은 null이거나 비어있을 수 없습니다.");
+        }
+    }
 }
